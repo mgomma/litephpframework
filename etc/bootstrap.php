761b1c8235a82ae2c3147365544e2a4447c226aa
NHC <nhc@NHCs-iMac.local>
2019-03-21 16:53:02 +0300

    Update bootstrap file

--- etc/bootstrap.php
+++ etc/bootstrap.php
@@ -14,13 +14,14 @@ define('MODULES_CLASS_DIRS', ['Controller', 'Model', 'Form', 'Drivers']);
 //define ('SUB_DIR', '/project')
 
 $envFilePath =  APP_DIR.DIRECTORY_SEPARATOR.'etc'.DIRECTORY_SEPARATOR.'env.php';
-define ('BASE_URL', $GLOBALS['env']['base_url']);
-
 if(file_exists($envFilePath)){
+
     include $envFilePath;
 }else{
     exit('Set env file to start testing');
+
 }
+define ('BASE_URL', $GLOBALS['env']['base_url']);
 
 $dir = new DirectoryIterator(APP_DIR.DIRECTORY_SEPARATOR.'core'.DIRECTORY_SEPARATOR.'src');
 foreach ($dir as $fileinfo) {
