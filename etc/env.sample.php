f3cc8bf69a27fe9f1be0620ce1ef3b51c811256e
NHC <nhc@NHCs-iMac.local>
2019-03-21 16:24:00 +0300

    env php sample

--- etc/env.sample.php
+++ etc/env.sample.php
@@ -1,9 +1,26 @@
-<?php 
+<?php
 
+$GLOBALS['env'] = [];
 
-$dbConfig = [
-  'database' => '',
-  'user' => '',
-  'password' => '',
-  'driver' => '',
+/*
+ * local
+ * stage
+ * prod
+ **/
+$GLOBALS['env']['mode'] = 'XXXX';
+
+$GLOBALS['env']['database'] = [
+    'host' => 'mariadb',
+    'name' => 'XXXX',
+    'user' => 'XXXX',
+    'password' => 'XXXX',
+    'driver' => 'mysql',
+    'port' => '3306',
+    'charset' => 'utf-8',
+];
+
+$GLOBALS['env']['unisms'] = [
+    'AppSid' => 'XXXX',
+    'url' => 'XXXX',
+    'SenderID' => 'XXXX'
 ];
